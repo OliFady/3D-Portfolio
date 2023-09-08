e93291f98fd110a3a5cbca1762165177bd7ae47c
OliFady <oliver20191702008@cis.asu.edu.eg>
2023-09-08 19:31:51 +0300

    Tech & Experience Sections

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import bosta from "./company/bosta.png";
+import banque from "./company/banque.png";
+import sparks from "./company/sparks.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  bosta,
+  banque,
+  sparks,
   carrent,
   jobit,
   tripguide,
